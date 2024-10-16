39f2ba22a260082efa60bffe289a31ce3437587e
tborjal <bor22014@byui.edu>
2024-10-16 16:19:12 +0800

    added product listing components and functions

---
+++ src/app/lib/definitions.ts
@@ -0,0 +1,9 @@
+export type Product = {
+    date: string | number | Date;
+    id: string;
+    title: string;
+    description: string;
+    price: number;
+    image: string;
+    category: string;
+  };
