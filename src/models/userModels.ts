a5440f5f95ce242ba69dc1c7cb835c1d78d6025e
Marcos-Antunes-Junior <mantunes140397@gmail.com>
2024-10-24 18:43:17 -0300

    Committing models.

--- src/models/userModels.ts
+++ src/models/userModels.ts
@@ -1,24 +1,46 @@
-import mongoose from "mongoose";
-import {ObjectId} from "mongodb";
+import mongoose from 'mongoose';
 
-let userSchema = new mongoose.Schema({
-_id: {type: ObjectId, auto: true},
-username:{
-    type: String,
-    required: true
-},
-email:{
-    type: String,
-    required: true,
-    unique: true
-},
-password:{
-   type: String,
-   required: true 
-}  
-})
+const sellerDetailsSchema = new mongoose.Schema({
+    category: {
+        type: String,
+        required: true,
+    },
+    business_name: {
+        type: String,
+        required: true,
+    },
+    bus_description: {
+        type: String,
+        required: true,
+    },
+});
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true, 
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    profile_img: {
+        type: String,
+        default: 'default.png', 
+    },
+    profile_description: {
+        type: String,
+        default: '', 
+    },
+    seller_details: sellerDetailsSchema, 
+}, { timestamps: true }); 
+
 
 const User = mongoose.models.users || mongoose.model('users', userSchema);
-
 
 export default User;
