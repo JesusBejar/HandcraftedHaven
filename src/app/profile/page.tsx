6b13995c9a41481d8af880a072f46b29fe8f1a2a
essunombre <alb21001@byui.edu>
2024-10-27 17:10:26 -0500

    adding id on local storage updating profile

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,21 +1,47 @@
 "use client";
-
 import React, { useEffect, useState } from 'react';
-import { fetchUser } from '../lib/mockData';
 import UserProfile from '../ui/profile/userProfile';
-import CommentsList from '../ui/comments/commentsList'; //To test the comments
-import { User } from '../lib/definitions'; 
+import CommentsList from '../ui/comments/commentsList'; // To test the comments
+import { User } from '../lib/definitions';
 
 export default function ProfilePage() {
   const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
-    const getUser = async () => {
-      const fetchedUser = await fetchUser();
-      setUser(fetchedUser);
+    const fetchUser = async () => {
+      const userId = localStorage.getItem("_id"); // Get user ID from localStorage
+      if (!userId) {
+        console.error("User ID not found in localStorage");
+        return;
+      }
+
+      try {
+        const response = await fetch(`/api/getUserByID?id=${userId}`);
+        const data = await response.json();
+
+        if (!data) {
+          console.error("No data received");
+          return;
+        }
+
+        setUser({
+          username: data.username || '',
+          email: data.email,
+          profile_img: data.profile_img,
+          profile_description: data.profile_description,
+          isSeller: data.is_seller,
+          categories: data.seller_details && data.seller_details.business_name 
+              ? [data.seller_details.category] 
+              : [],
+          business_name: data.seller_details ? data.seller_details.business_name : '',
+          bus_description: data.seller_details ? data.seller_details.bus_description : ''
+        });
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
     };
 
-    getUser();
+    fetchUser();
   }, []);
 
   if (!user) {
@@ -25,7 +51,7 @@ export default function ProfilePage() {
   return (
     <>
       <UserProfile user={user} />
-      <CommentsList /> {/* This is only for testing, meake sure you use it on card*/}
+      <CommentsList /> {/* This is only for testing; make sure to use it on card */}
     </>
   );
-};
+}
