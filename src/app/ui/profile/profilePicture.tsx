5fd56c3f9937a50c6a1eac89a75cc220a324c9c9
Marcos-Antunes-Junior <mantunes140397@gmail.com>
2024-10-29 17:39:29 -0300

    Fixed profilePicture.

--- src/app/ui/profile/profilePicture.tsx
+++ src/app/ui/profile/profilePicture.tsx
@@ -1,5 +1,8 @@
-export default ProfilePicture: React.FC<{ src: string; alt: string }> = ({
+import React from 'react';
+
+const ProfilePicture: React.FC<{ src: string; alt: string }> = ({
   src,
   alt,
 }) => <img src={src} alt={alt} className="profile-picture" />;
 
+export default ProfilePicture;
